368dc7b0f05e5e29b407ba39b61f70393e584f6e
wikb <2292518295@qq.com>
2022-05-13 09:54:56 +0800

    第二次更新项目

---
+++ src/views/home/Home.vue
@@ -0,0 +1,43 @@
+<template>
+  <div id="home">
+    <nav-bar class="home-nav"><div slot="center">购物街</div></nav-bar>
+    <home-swiper :banners="banners"></home-swiper>
+    <recommend-view :recommends="recommends"></recommend-view>
+  </div>
+</template>
+
+<script>
+import NavBar from "@/components/common/navbar/NavBar.vue"
+import { getHomeMultidata } from "@/network/home"
+import HomeSwiper from "./childComps/HomeSwiper.vue"
+import RecommendView from "./childComps/RecommendView.vue"
+
+export default {
+  name: "Home",
+  components: {
+    NavBar,
+    HomeSwiper,
+    RecommendView
+  },
+  data() {
+    return {
+      banners: [],
+      recommends: [],
+    }
+  },
+  created() {
+    getHomeMultidata().then(res => {
+      // this.result = res;
+      this.banners = res.data.banner.list;
+      this.recommends = res.data.recommend.list;
+    })
+  }
+}
+</script>
+
+<style>
+  .home-nav {
+    background-color: var(--color-tint);
+    color: #fff;
+  }
+</style>
