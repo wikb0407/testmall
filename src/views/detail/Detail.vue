7378abac048085533934269e22732b9fd003275f
wikb <2292518295@qq.com>
2022-05-31 09:24:02 +0800

    第三次项目更新

--- src/views/detail/Detail.vue
+++ src/views/detail/Detail.vue
@@ -1,24 +1,107 @@
 <template>
-  <div>
-    {{iid}}
-    <div>详情页</div>
+  <div id="detail">
+    <detail-nav-bar class="detail-nav"/>
+    <scroll class="content" ref="scroll">
+      <detail-swiper :top-images= "topImages"/>
+      <detail-base-info :goods="goods"/>
+      <detail-shop-info :shop="shop"/>
+      <detail-goods-info :detailInfo="detailInfo" @imageLoad="imageLoad"/>
+      <detail-param-info :param-info="paramInfo"/>
+      <detail-comment-info :commentInfo="commentInfo" />
+      <goods-list :goods="recommends"/>
+    </scroll>
   </div>
 </template>
 
 <script>
+import DetailNavBar from './childComps/DetailNavBar.vue'
+import DetailSwiper from './childComps/DetailSwiper.vue'
+import DetailBaseInfo from './childComps/DetailBaseInfo.vue'
+import DetailShopInfo from './childComps/DetailShopInfo.vue'
+import DetailGoodsInfo from './childComps/DetailGoodsInfo.vue'
+import DetailParamInfo from './childComps/DetailParamInfo.vue'
+import DetailCommentInfo from './childComps/DetailCommentInfo.vue'
+
+import Scroll from '@/components/common/scroll/Scroll.vue'
+import GoodsList from '@/components/content/goods/GoodsList.vue'
+
+import {getDetail, getRecommend, Goods, Shop, GoodsParam } from '@/network/detail'
+
 export default {
   name: "Detail",
+  components: {
+    DetailNavBar,
+    DetailSwiper,
+    DetailBaseInfo,
+    DetailShopInfo,
+    DetailGoodsInfo,
+    DetailParamInfo,
+    DetailCommentInfo,
+    Scroll,
+    GoodsList
+  },
   data() {
     return {
-      iid: null
+      iid: null,
+      topImages: [],
+      goods: {},
+      shop: {},
+      detailInfo: {},
+      paramInfo: {},
+      commentInfo: {},
+      recommends: []
     }
   },
   created() {
+    // 保存传入的id
     this.iid = this.$route.params.iid
+    // 根据iid 请求详情数据
+    getDetail(this.iid).then(res => {
+      console.log(res);
+      const data = res.result
+      this.topImages = data.itemInfo.topImages
+
+      // 获取商品信息
+      this.goods = new Goods(data.itemInfo,data.columns,data.shopInfo.services)
+      // 创建店铺信息的对象
+      this.shop = new Shop(data.shopInfo)
+      // 保存商品的详情数据
+      this.detailInfo = data.detailInfo;
+      // 获取参数信息
+      this.paramInfo = new GoodsParam(data.itemParams.info, data.itemParams.rule) 
+      //取出评论信息
+      if (data.rate.cRate !== 0) {
+        this.commentInfo = data.rate.list[0]
+      } 
+    })
+    getRecommend().then(res => {
+      console.log(res);
+      this.recommends = res.data.list
+    })
+
+  },
+  methods: {
+    imageLoad() {
+      this.$refs.scroll.refresh()
+    }
   }
 }
 </script>
 
-<style>
-
+<style scoped>
+  #detail {
+    position: relative;
+    z-index: 9;
+    background-color: #fff;
+    height: 100vh;
+  }
+  .content {
+    height: calc(100% - 44px);
+    overflow: hidden;
+  }
+  .detail-nav {
+    position: relative;
+    z-index: 10;
+    background-color: #fff;
+  }
 </style>
